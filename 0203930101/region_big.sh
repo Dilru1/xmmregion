52813ff6f646b9f2e3ef402940b2ce61ffa2cf08
Dilruwan <dilrushaka@gmail.com>
2025-04-03 20:30:13 +0200

    first commit for dev

--- 0203930101/region_big.sh
+++
@@ -1,3 +0,0 @@
-esky2det datastyle=user ra=266.85523703897144 dec=-28.428618895639474 outunit=det withheader=no calinfostyle=set calinfoset=pnS003-obj-image-sky.fits >> reg_box1.txt 
-esky2det datastyle=user ra=266.85523703897144 dec=-28.428618895639474 outunit=det withheader=no calinfostyle=set calinfoset=mos1S001-obj-image-sky.fits >> reg_box1.txt 
-esky2det datastyle=user ra=266.85523703897144 dec=-28.428618895639474 outunit=det withheader=no calinfostyle=set calinfoset=mos2S002-obj-image-sky.fits >> reg_box1.txt 
